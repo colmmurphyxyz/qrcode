2c8a4fbadea439038e296a179f8d49d3381f1192
Colm Murphy <colmmurphy016@gmail.com>
2023-11-03 14:41:29 +0000

    Remove some of the unnecessary GUI code

--- src/example/QRCodeDecoderExample.java
+++ src/example/QRCodeDecoderExample.java
@@ -4,25 +4,21 @@ import jp.sourceforge.qrcode.QRCodeDecoder;
 import jp.sourceforge.qrcode.util.ContentConverter;
 import jp.sourceforge.qrcode.data.QRCodeImage;
 import jp.sourceforge.qrcode.exception.DecodingFailedException;
-import jp.sourceforge.qrcode.exception.InvalidVersionInfoException;
 
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-import jp.sourceforge.qrcode.util.DebugCanvas;
-import jp.sourceforge.qrcode.util.DebugCanvasAdapter;
-
 import java.net.URL;
 
 // Smallest example of QRCode Decoder
 
-public class QRCodeDecoderCUIExample {
+public class QRCodeDecoderExample {
 
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.err.println("Usage: QRCodeDecoderCUIExample imageFilePath");
+            System.err.println("Usage: QRCodeDecoderExample imageFilePath");
             System.exit(1);
         }
         int numSuccesses = 0;
@@ -37,7 +33,7 @@ public class QRCodeDecoderCUIExample {
                 System.err.print("[Failure] ");
                 numFailures++;
             }
-            System.err.println(filename);
+            System.err.println(filename + "\n");
         }
         long processTime = System.currentTimeMillis() - start;
         System.err.println("Processed " + args.length + " images in " + processTime + "ms (" + processTime / args.length + " images/sec)");
@@ -45,8 +41,6 @@ public class QRCodeDecoderCUIExample {
     }
 
     static boolean processDecode(String filename, QRCodeDecoder decoder) {
-        DebugCanvas canvas = new J2SECanvas();
-        decoder.setCanvas(canvas);
         BufferedImage image;
         try {
             if (filename.startsWith("http://") || filename.startsWith("https://")) {
@@ -58,11 +52,9 @@ public class QRCodeDecoderCUIExample {
             decodedString = ContentConverter.convert(decodedString);
             System.out.println(decodedString);
         } catch (IOException e) {
-            canvas.println("Error: " + e.getMessage() + " " + filename);
             System.out.println("Error: " + e.getMessage() + " " + filename);
             return false;
         } catch (DecodingFailedException dfe) {
-            canvas.println("Error: " + dfe.getMessage());
             System.out.println("Error: " + dfe.getMessage());
             return false;
         } catch (Exception e) {
@@ -91,11 +83,5 @@ class J2SEImage implements QRCodeImage {
     public int getPixel(int x, int y) {
         return image.getRGB(x, y);
 
-    }
-}
-
-class J2SECanvas extends DebugCanvasAdapter {
-    public void println(String s) {
-        //System.err.println(s);
     }
 }
