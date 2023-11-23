6e8d54aac519034dc4eb18d8897405e05ffcaa84
Colm Murphy <colmmurphy016@gmail.com>
2023-11-23 13:05:19 +0000

    API and example application

---
+++ src/example/DecoderExample.java
@@ -0,0 +1,40 @@
+package example;
+
+import jp.sourceforge.qrcode.DecodeResult;
+import jp.sourceforge.qrcode.Decoder;
+import jp.sourceforge.qrcode.QRCodeDecoder;
+import jp.sourceforge.qrcode.data.QRCodeSymbol;
+import jp.sourceforge.qrcode.util.DebugCanvasAdapter;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class DecoderExample {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("please pass a QR Code image filepath as an argument");
+            System.err.println("e.g java -jar qrcode.jar qrcode.png");
+        }
+        Decoder decoder = new Decoder();
+        Decoder.setCanvas(new DefaultCanvas());
+        for (String filename : args) {
+            try {
+                BufferedImage image = ImageIO.read(new File(filename));
+                byte[] result = decoder.decodeImage(new J2SEImage(image));
+                String decodedText = new String(result);
+                System.out.println(decodedText);
+            } catch (IOException e) {
+                System.err.println("ERROR: Couldn't read " + filename);
+            }
+        }
+    }
+}
+
+class DefaultCanvas extends DebugCanvasAdapter {
+    public void println(String s) {
+        System.out.println(s);
+    }
+}
+
